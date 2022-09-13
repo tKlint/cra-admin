f0fb95e98554abb476de457a17e77fa0fd0234c9
tklint <kuicode0066@163.com>
2022-09-13 18:32:57 +0800

    feat: 判斷登錄狀態

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React, { Suspense, useEffect } from "react";
 import { RouteObject, useLocation, useNavigate, useRoutes } from "react-router-dom";
 import WrapperRouteComponent from './WrapperRouteComponent';
 import { useAppSelector } from '../store/hooks';
+import storage from '../utils/Storage';
 
 const NotFound = React.lazy(() => import("../pages/404"));
 const Login = React.lazy(() => import("../pages/login"));
@@ -24,15 +25,16 @@ const defaultRouters: RouteObject[] = [
 ];
 
 export default function DynamicRouter() {
-	console.log(process.env, 'createStorage')
 	const user = useAppSelector((state) => state.userReducer);
   const { pathname, state } = useLocation();
 	const { token } = user;
 	const navigate = useNavigate();
-
   useEffect(() => {
     if (!token && pathname !== '/login') {
       return navigate({ pathname: 'login' }, { replace: true, state: { from: pathname } });
+		}
+		if (token && storage.get('access_token', '') && pathname === '/login') {
+			return navigate({ pathname: '/' });
 		}
   }, [token, navigate, pathname, state]);
 
