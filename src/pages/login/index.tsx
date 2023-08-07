602d3bc49e47c38f13667b7550aa8c4c461f9745
葛中魁 <gezhongkui@gezhongkuideMac-mini.local>
2023-08-07 17:02:39 +0800

    feat(login): 添加looding

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,16 +2,19 @@ import { Button } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import { useAppDispatch } from '../../store/hooks';
 import { fetchUser } from '../../store/user';
+import { useState } from 'react';
 
 export default function Login() {
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
+  const loginHandle = async () => {
+    setLoading(true);
+    await dispatch(fetchUser());
+    setLoading(false);
+  };
   return (
     <div>
-      <Button
-        onClick={() => {
-          dispatch(fetchUser());
-        }}
-      >
+      <Button loading={loading} onClick={loginHandle}>
         <FormattedMessage id="page.login.login" />
       </Button>
     </div>
